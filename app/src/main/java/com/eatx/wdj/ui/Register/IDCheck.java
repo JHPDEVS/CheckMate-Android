31f90a3e5ea94afa03c9b326e295ea997624a8f1
JHPDEVS <jhpdevs@g.yju.ac.kr>
2021-06-21 03:07:48 +0900

    -check id and all inputed

---
+++ app/src/main/java/com/eatx/wdj/ui/Register/IDCheck.java
@@ -0,0 +1,34 @@
+package com.eatx.wdj.ui.Register;
+
+
+import androidx.annotation.Nullable;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Response;
+import com.android.volley.toolbox.StringRequest;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class IDCheck extends StringRequest {
+
+    final static private String URL = "https://ckmate.shop/.well-known/dupiID.php";
+
+    private Map<String, String> map;
+
+    public IDCheck(String id ,Response.Listener<String> listener) {
+        super(Method.POST, URL,listener,null);
+        map = new HashMap<>();
+        map.put("id",id);
+//        map.put("sid",sid +"");
+//        map.put("name",name);
+//        map.put("phoneNumber",phoneNumber+"");
+//        map.put("email",email);
+    }
+
+    @Override
+    protected Map<String, String> getParams() throws AuthFailureError {
+        return map;
+    }
+}
--- app/src/main/java/com/eatx/wdj/ui/Register/Register.java
+++ app/src/main/java/com/eatx/wdj/ui/Register/Register.java
@@ -18,6 +18,7 @@ import com.android.volley.Response;
 import com.android.volley.toolbox.Volley;
 import com.eatx.wdj.R;
 import com.eatx.wdj.ui.login.LoginActivity;
+import com.eatx.wdj.ui.login.LoginRequest;
 import com.eatx.wdj.ui.login.MainActivity;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -26,6 +27,7 @@ import androidx.appcompat.widget.AppCompatTextView;
 
 import com.eatx.wdj.ui.main.PhoneAuth;
 import com.google.android.material.snackbar.Snackbar;
+import com.royrodriguez.transitionbutton.TransitionButton;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -39,8 +41,8 @@ import java.sql.SQLException;
 public class Register extends AppCompatActivity {
 
     private EditText et_phone ,et_id ,et_password ,et_name ,et_email,et_password2,et_sid,et_class;
-    private TextView registerButton;
     private AppCompatImageView BackButton;
+    private TransitionButton registerButton;
     Boolean dupi = false;
 
     @Override
@@ -76,41 +78,7 @@ public class Register extends AppCompatActivity {
         registerButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String id = et_id.getText().toString();
-                String password = et_password.getText().toString();
-                String name = et_name.getText().toString();
-                int sid = Integer.parseInt(et_sid.getText().toString());
-                String email = et_email.getText().toString();
-                int phone = Integer.parseInt(et_phone.getText().toString());
-                String classValue = et_class.getText().toString();
-                Response.Listener<String> responseListener = new Response.Listener<String>() {
-                    @Override
-                    public void onResponse(String response) {
-                        try {
-                            JSONObject jsonObject = new JSONObject(response);
-                            boolean success = jsonObject.getBoolean("success");
-                            if(success) {
-                                Toast.makeText(getApplicationContext(),"회원가입에 성공했습니다",Toast.LENGTH_SHORT).show();
-                                Intent intent = new Intent(Register.this,MainActivity.class);
-                                intent.putExtra("id",id);
-                                intent.putExtra("password",password);
-                                startActivity(intent);
-                            } else {
-                                Snackbar snackbar = Snackbar.make(findViewById(android.R.id.content),"회원가입에 실패했습니다",Snackbar.LENGTH_SHORT);
-                                snackbar.setAnchorView(findViewById(R.id.goRegister));
-                                snackbar.show();
-                                return;
-                            }
-                        } catch (JSONException e) {
-                            e.printStackTrace();
-                        }
-                    }
-                };
-                // 서버로 Volley를 이용해서 요청함
-              RegisterRequest registerRequest = new RegisterRequest(id,password,sid,name,classValue,phone,email,responseListener);
-              RequestQueue queue = Volley.newRequestQueue(Register.this);
-              queue.add(registerRequest);
-
+                dupiID();
             }
         });
 
@@ -121,12 +89,97 @@ public class Register extends AppCompatActivity {
             }
         });
     }
+    private void dupiID() {
+        String id = et_id.getText().toString();
+        registerButton.startAnimation();
+        Response.Listener<String> responseListener = new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                try {
+                    JSONObject jsonObject = new JSONObject(response);
+                    boolean success = jsonObject.getBoolean("success");
+                    System.out.println(success);
+                    if(success) {
+                        registerButton.stopAnimation(TransitionButton.StopAnimationStyle.SHAKE, null);
+                        Snackbar snackbar = Snackbar.make(findViewById(android.R.id.content),"중복된 아이디입니다",Snackbar.LENGTH_SHORT);
+                        snackbar.setAnchorView(findViewById(R.id.goRegister));
+                        snackbar.show();
+                        return;
+                    } else {
+                        checkALL();
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        IDCheck dupiID = new IDCheck(id,responseListener);
+        RequestQueue queue = Volley.newRequestQueue(Register.this);
+        queue.add(dupiID);
+    }
+    private void checkALL() {
+        if(et_id.getText().toString().equals("") || et_name.getText().toString().equals("") || et_password.getText().toString().equals("") || et_password2.getText().toString().equals("")
+        || et_class.getText().toString().equals("") || et_email.getText().toString().equals("") || et_sid.getText().toString().equals("")) {
+            registerButton.stopAnimation(TransitionButton.StopAnimationStyle.SHAKE, null);
+            Snackbar snackbar = Snackbar.make(findViewById(android.R.id.content),"빈틈없이 입력해주세요",Snackbar.LENGTH_SHORT);
+            snackbar.setAnchorView(findViewById(R.id.goRegister));
+            snackbar.show();
+            return;
+        } else {
+            if(et_password.getText().toString().equals(et_password2.getText().toString())) {
+                register();
+            } else {
+                registerButton.stopAnimation(TransitionButton.StopAnimationStyle.SHAKE, null);
+                Snackbar snackbar = Snackbar.make(findViewById(android.R.id.content),"비밀번호 확인값이랑 비밀번호가 동일하지 않습니다.",Snackbar.LENGTH_SHORT);
+                snackbar.setAnchorView(findViewById(R.id.goRegister));
+                snackbar.show();
+                return;
+            }
+        }
+    }
+
     private void getPhoneNo() {
         Intent intent = getIntent();
         String phoneNo = intent.getStringExtra("phoneNo").substring(3);
         et_phone.setText("0"+phoneNo);
     }
+    private void register() {
+        String id = et_id.getText().toString();
+        String password = et_password.getText().toString();
+        String name = et_name.getText().toString();
+        int sid = Integer.parseInt(et_sid.getText().toString());
+        String email = et_email.getText().toString();
+        int phone = Integer.parseInt(et_phone.getText().toString());
+        String classValue = et_class.getText().toString();
+        Response.Listener<String> responseListener = new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                try {
+                    JSONObject jsonObject = new JSONObject(response);
+                    boolean success = jsonObject.getBoolean("success");
+                    if(success) {
+                        Toast.makeText(getApplicationContext(),"회원가입에 성공했습니다",Toast.LENGTH_SHORT).show();
+                        Intent intent = new Intent(Register.this,MainActivity.class);
+                        intent.putExtra("id",id);
+                        intent.putExtra("password",password);
+                        startActivity(intent);
+                    } else {
+                        Snackbar snackbar = Snackbar.make(findViewById(android.R.id.content),"회원가입에 실패했습니다",Snackbar.LENGTH_SHORT);
+                        snackbar.setAnchorView(findViewById(R.id.goRegister));
+                        snackbar.show();
+                        return;
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        // 서버로 Volley를 이용해서 요청함
+        RegisterRequest registerRequest = new RegisterRequest(id,password,sid,name,classValue,phone,email,responseListener);
+        RequestQueue queue = Volley.newRequestQueue(Register.this);
+        queue.add(registerRequest);
 
+    }
     private boolean isName() {
         return dupi;
     }
